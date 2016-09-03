bb403a28bb52ff7f18c12e0835c6e10f97ad78e3
Ela Tan <ela.tan@gmail.com>
2016-09-02 19:07:07 -0700

    Ajaxify Likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,28 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   def create
-    product = Product.find params[:product_id]
-    review = Review.find params[:review_id]
-    like = Like.new user: current_user, review: review
-    if like.save
-      redirect_to product_path(product), notice: "Thanks for the like"
-    else
-      redirect_to product_path(product), alert: like.errors.full_messages.join(", ")
+    @product = Product.find params[:product_id]
+    @review = Review.find params[:review_id]
+    @like = Like.new user: current_user, review: @review
+    respond_to do |format|
+      if @like.save
+        format.html {redirect_to product_path(@review.product_id), notice: "Thanks for the like"}
+        format.js { render }
+      else
+        format.html {redirect_to product_path(@review.product_id), alert: like.errors.full_messages.join(", ")}
+        format.js {render}
+      end
     end
   end
 
   def destroy
-    product = Product.find params[:product_id]
-    review = Review.find params[:review_id]
-    like = Like.find params[:id]
-    like.destroy
-    redirect_to product_path(product), notice: "like removed"
+    @product = Product.find params[:product_id]
+    @review = Review.find params[:review_id]
+    @like = Like.find params[:id]
+    @like.destroy
+    respond_to do |format|
+      format.html {redirect_to product_path(@product), notice: "like removed"}
+      format.js { render }
+    end
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,18 +4,31 @@ class ReviewsController < ApplicationController
     @review = Review.new params.require(:review).permit(:body, :rating)
     @product = Product.find params[:product_id]
     @review.product = @product
-    if @review.save
-      redirect_to product_path(@product), notice: "Review created!"
-    else
-      flash[:alert] = "Please fix errors below"
-      render "/products/show"
+
+    respond_to do |format|
+
+      if @review.save
+        format.html {redirect_to product_path(@product), notice: "Review created!"}
+        format.js {render :create_success}
+      else
+        flash[:alert] = "Please fix errors below"
+        format.html {render "/products/show"}
+        format.js {render :create_failure}
+      end
     end
   end
 
   def destroy
     a = Product.find params[:product_id]
-    r = Review.find params[:id]
-    r.destroy
-    redirect_to product_path(a), notice: "Review deleted!"
+    # r = Review.find params[:id]
+    # r.destroy
+    @review = Review.find params[:id]
+    @review.destroy
+
+    respond_to do |format|
+      format.html {redirect_to product_path(a), notice: "Review deleted!"}
+      format.js {render}
+    end
+
   end
 end
